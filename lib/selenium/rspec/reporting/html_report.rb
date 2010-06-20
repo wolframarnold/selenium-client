90b43bfe0b75b9e58ecd2f06d040023f9501777f
Joshua Krall <joshuakrall@pobox.com>
2010-06-19 19:50:25 -0500

    bugfix for backtrace tweaker

--- lib/selenium/rspec/reporting/html_report.rb
+++ lib/selenium/rspec/reporting/html_report.rb
@@ -1,7 +1,7 @@
 module Selenium
   module RSpec
     module Reporting
-            
+
       class HtmlReport
 
         PLACEHOLDER = "<<placeholder>>"
@@ -13,18 +13,18 @@ module Selenium
         def self.inject_placeholder(content)
           content + Selenium::RSpec::Reporting::HtmlReport::PLACEHOLDER
         end
-        
+
         def replace_placeholder_with_system_state_content(result, example)
           result.gsub! PLACEHOLDER, logs_and_screenshot_sections(example)
         end
-        
+
         def logs_and_screenshot_sections(example)
           dom_id = "example_" + example.reporting_uid
           system_screenshot_url = @file_path_strategy.relative_file_path_for_system_screenshot(example)
           page_screenshot_url = @file_path_strategy.relative_file_path_for_page_screenshot(example)
           snapshot_url = @file_path_strategy.relative_file_path_for_html_capture(example)
           remote_control_logs_url = @file_path_strategy.relative_file_path_for_remote_control_logs(example)
-          
+
           html = ""
           if File.exists? @file_path_strategy.file_path_for_html_capture(example)
             html << toggable_section(dom_id, :id => "snapshot", :url=> snapshot_url, :name => "Dynamic HTML Snapshot")
@@ -38,7 +38,7 @@ module Selenium
           if File.exists? @file_path_strategy.file_path_for_system_screenshot(example)
             html << toggable_image_section(dom_id, :id => "system_screenshot", :name => "System Screenshot", :url => system_screenshot_url)
           end
-          
+
           return html
         end
 
@@ -64,7 +64,7 @@ module Selenium
 
         def self.append_css(global_styles)
             global_styles + <<-EOF
-            
+
         div.rspec-report textarea {
           width: 100%;
         }
@@ -77,15 +77,15 @@ module Selenium
           margin: 0.5em 2em;
           padding: 0.5em;
         }
-        
+
         EOF
         end
 
         def toggable_section(dom_id, options)
           <<-EOS
-          
+
           <div>[
-            <a id="#{dom_id}_#{options[:id]}_link" 
+            <a id="#{dom_id}_#{options[:id]}_link"
                href=\"javascript:toggleVisilibility('#{dom_id}_#{options[:id]}', '#{options[:name]}')\">Show #{options[:name]}</a>
           ]</div>
           <br/><br/>
@@ -93,37 +93,37 @@ module Selenium
             <a href="#{options[:url]}">Full screen</a><br/><br/>
             <iframe src="#{options[:url]}" width="100%" height="600px" ></iframe>
           </div>
-          
+
           EOS
         end
-        
+
         def toggable_image_section(dom_id, options)
           <<-EOS
-          
+
           <div>[<a id="#{dom_id}_#{options[:id]}_link" href="javascript:toggleVisilibility('#{dom_id}_#{options[:id]}', '#{options[:name]}');">Show #{options[:name]}</a>]</div>
-          <br/>      
+          <br/>
           <div id="#{dom_id}_#{options[:id]}" style="display: none">
             <a href="#{options[:url]}">
               <img width="80%" src="#{options[:url]}" />
             </a>
           </div>
           <br/>
-          
+
           EOS
         end
-        
+
         def report_header
           super + "\n<script type=\"text/javascript\">moveProgressBar('100.0');</script>"
         end
 
-      end      
+      end
     end
-    
+
 
     # Tweaks raised Exceptions to mask noisy (unneeded) parts of the backtrace
     class SeleniumQuietBacktraceTweaker < Spec::Runner::QuietBacktraceTweaker
       unless defined?(SELENIUM_IGNORE_PATTERNS)
-        SELENIUM_IGNORE_PATTERNS = spec_files + [
+        SELENIUM_IGNORE_PATTERNS = [
           /selenium-client/,
           /selenium\/client/,
         ]
